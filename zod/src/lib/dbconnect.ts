853ebcca7a0af9aa9e07840a3f756c49059c2315
kamboj7791 <kambojkunal01@gmail.com>
2024-05-01 01:21:46 +0530

    db connection

---
+++ zod/src/lib/dbconnect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isconnected?: number;
+};
+const connection: connectionObject = {};
+const dbconnect = async (): Promise<void> => {
+  if (connection.isconnected) {
+    console.log("Already connected to database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    console.log(db);
+    connection.isconnected = db.connections[0].readyState;
+    console.log(db.connections);
+    console.log("DB connected successfully");
+  } catch (error: any) {
+    console.log("failed to connect to database", error);
+    process.exit(1);
+  }
+};
